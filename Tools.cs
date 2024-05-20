86285c0b20d0c1a5994c8c0cd4e6aa14de172eb6
mohammad amin ghasemi <mmnghasemi@gmail.com>
2024-05-20 10:53:31 +0330

    Pops : Temp[] , Index , CurrentPosition Added Inisialized in the Ctor; Insert Method Coded

--- Tools.cs
+++ Tools.cs
@@ -2,19 +2,31 @@
 {
     public class Tools : ITools
     {
+        public string[] Temp { get; set; }
+        public int Index { get; set; }
+        public int CurrentPosition { get; set; }
+
+        public Tools()
+        {
+            Temp = new string[200];     // i can use Temp.Lenght too but it may cost too much memory !
+            Index = 0;
+            CurrentPosition = 0;
+        }
         public void Insert(string str)
         {
-            throw new NotImplementedException();
+            Temp[Index] = str;
+            CurrentPosition = Index;
+            Index++;
         }
 
         public void Redo()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Undo()
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
