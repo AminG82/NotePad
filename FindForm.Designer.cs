d142a90cc7ae01c31afe788fdb578c7dc0727134
mohammad amin ghasemi <mmnghasemi@gmail.com>
2024-05-25 12:46:38 +0330

    FindForm added and designed

---
+++ FindForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace NotePad
+{
+    partial class FindForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtFind = new TextBox();
+            btnFind = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // txtFind
+            // 
+            txtFind.Location = new Point(87, 34);
+            txtFind.Name = "txtFind";
+            txtFind.Size = new Size(291, 27);
+            txtFind.TabIndex = 0;
+            // 
+            // btnFind
+            // 
+            btnFind.Location = new Point(394, 34);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(94, 29);
+            btnFind.TabIndex = 1;
+            btnFind.Text = "Find";
+            btnFind.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Find";
+            // 
+            // FindForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(500, 104);
+            Controls.Add(label1);
+            Controls.Add(btnFind);
+            Controls.Add(txtFind);
+            Name = "FindForm";
+            Text = "FindForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtFind;
+        private Button btnFind;
+        private Label label1;
+    }
+}
